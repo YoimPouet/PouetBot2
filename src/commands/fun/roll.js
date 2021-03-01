db70782288dd21de1c9ca7ac963ea9bf2d8de161
YoimPouet <65048905+YoimPouet@users.noreply.github.com>
2021-03-01 15:30:44 +0100

    Delete roll.js

--- src/commands/fun/roll.js
+++
@@ -1,9 +0,0 @@
-const rollDice = () => Math.floor(Math.random() * 6) + 1;
-
-module.exports = {
-    run: async(client, message) => {
-        message.reply("rolled a " + rollDice());
-    },
-    aliases: ['dice', 'rolldice'],
-    description: 'Rolls the dice'
-}
